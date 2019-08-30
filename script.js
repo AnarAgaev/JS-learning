890dbb2833f2062b95e9861707e381b345c51cf2
Anar Agaev <anar.n.agaev@gmail.com>
2019-08-30 18:38:42 +0300

    arrays

--- script.js
+++ script.js
@@ -1,34 +1,72 @@
 'use strict'
 
-let ob = new Object(),
-    options = {
-        widht: 1024,
-        heigh: 720,
-        name: "test",
-    };
+// let arr = [1, 2, 3, 'four', 5];
+// console.log(arr);
 
-console.log(options.name);
+// arr.pop();
+// console.log(arr);
+// arr.push('5');
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.unshift('first');
+// console.log(arr);
 
-options.bool = false;
-options.colors = {
-    border: 'black',
-    bg: 'red',
-}
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(typeof arr[i] + ' - ' + arr[i]);
+// }
 
-console.log(options);
+// arr.forEach(function (item, i, mass) {
+//     console.log(i + ': ' + item + ' (массив: ' + mass + ')');    
+// });
 
-function printObj(object) {
-    for (let key in object) {
-        if (typeof object[key] == 'object') {
-            console.log(' Inner Object: ');
-            printObj(object[key]);
-            console.log('');
-            
-        } else {
-            console.log(key + ': ' + object[key]);
-        }
-    }
-}
-printObj(options);
+// let newArr = [1, 3, 4, 6, 7];
+// for (let key of newArr) {
+//     console.log(key);
+// }
 
-console.log(Object.keys(options).length);
+// let answear = prompt('', ''),
+//     arr = [];
+// arr = answear.split(',');
+// console.log(arr);
+
+// let arr = ['asdfa','rqwerq','ertyer','ugfyu'],
+//     i = arr.join(', ');
+// console.log(i);
+
+// let arr = ['1','15','4'];
+
+// arr.sort(copareNum);
+
+// function copareNum(a, b) {
+//     return a-b;
+// }
+// console.log(arr);
+
+// let fruits = ['Яблоко', 'Банан'],
+//     first = fruits[0],
+//     last = fruits[fruits.length - 1];
+
+// fruits.push('Апельсин');
+// fruits.pop();
+// fruits.shift();
+// fruits.unshift('Клубника');
+// fruits.push('Манго');
+// fruits.splice(0, 1);
+
+// console.log(fruits);
+// console.log(fruits.indexOf('Манго'));
+
+// let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'],
+//     removedItems = vegetables.splice(1, 2);
+
+// console.log(vegetables);
+// console.log(removedItems);
+
+
+let array = new Array(5);
+
+array.forEach(function (element) {
+    console.log('asd' + element);
+    
+});
