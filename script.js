346cb1a7eb3a5f5eb5979921b5ee9a992b4b2bd4
Anar Agaev <anar.n.agaev@gmail.com>
2019-09-01 17:51:56 +0300

    object codding exapmle

--- script.js
+++ script.js
@@ -1,72 +1,32 @@
 'use strict'
 
-// let arr = [1, 2, 3, 'four', 5];
-// console.log(arr);
+let soldier = {
+	halth: 400,
+	armor: 150,
+};
 
-// arr.pop();
-// console.log(arr);
-// arr.push('5');
-// console.log(arr);
-// arr.shift();
-// console.log(arr);
-// arr.unshift('first');
-// console.log(arr);
+let john = {
+	halth: 200,
+};
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(typeof arr[i] + ' - ' + arr[i]);
-// }
-
-// arr.forEach(function (item, i, mass) {
-//     console.log(i + ': ' + item + ' (массив: ' + mass + ')');    
-// });
-
-// let newArr = [1, 3, 4, 6, 7];
-// for (let key of newArr) {
-//     console.log(key);
-// }
-
-// let answear = prompt('', ''),
-//     arr = [];
-// arr = answear.split(',');
-// console.log(arr);
-
-// let arr = ['asdfa','rqwerq','ertyer','ugfyu'],
-//     i = arr.join(', ');
-// console.log(i);
-
-// let arr = ['1','15','4'];
-
-// arr.sort(copareNum);
-
-// function copareNum(a, b) {
-//     return a-b;
-// }
-// console.log(arr);
-
-// let fruits = ['Яблоко', 'Банан'],
-//     first = fruits[0],
-//     last = fruits[fruits.length - 1];
-
-// fruits.push('Апельсин');
-// fruits.pop();
-// fruits.shift();
-// fruits.unshift('Клубника');
-// fruits.push('Манго');
-// fruits.splice(0, 1);
-
-// console.log(fruits);
-// console.log(fruits.indexOf('Манго'));
-
-// let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'],
-//     removedItems = vegetables.splice(1, 2);
-
-// console.log(vegetables);
-// console.log(removedItems);
+john.__proto__ = soldier;
+console.log(john);
+console.log(john.armor);
 
 
-let array = new Array(5);
 
-array.forEach(function (element) {
-    console.log('asd' + element);
-    
-});
+class User {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHi() {
+    console.log(this.name);
+  }
+
+}
+
+// Использование:
+let user = new User("Иван");
+user.sayHi();
