3b2bdcc96890a6cf31638128b24a0c33e163397e
Anar Agaev <anar.n.agaev@gmail.com>
2019-09-28 17:32:58 +0300

    Добавил калькулятор

--- yoga/script.js
+++ yoga/script.js
@@ -167,13 +167,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
     // Slider
-
     let slideIndex = 1, // Number of active slide
         slides = document.querySelectorAll('.slider-item'), // All sliders we need slide
         prev = document.querySelector('.prev'), // Button prev
         next = document.querySelector('.next'), // Button next
         dotsWrap = document.querySelector('.slider-dots'),  // Wrapper for dot buttons
         dots = dotsWrap.querySelectorAll('.dot'); // All dot button for sliding
+
+    showSlide(slideIndex);
 
     /**
      * @param {n} - number of slide 
@@ -245,7 +246,49 @@ window.addEventListener('DOMContentLoaded', function () {
         item.classList.remove(cls);
     }
 
-    showSlide(slideIndex);
+
+    // Calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerHTML = 0; // Сразу обнуляем результат вычислений калькулятора
+
+    persons.addEventListener('change', function() {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    restDays.addEventListener('change', function() {
+        daysSum = +this.value;
+        total = daysSum * personsSum * 10000;
+
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function() {
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
 });
 
 
