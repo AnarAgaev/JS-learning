8654e7707042626f0333e0d9daf271c6aa831a51
Anar Agaev <anar.n.agaev@gmail.com>
2019-10-02 19:44:07 +0300

    Геттеры и Сеттеры

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 'use strict';
 
-let json = '{"id":2,}';
+function User(name, age) {
+	this.name = name;
+	let userAge = age;
 
-try {
-	let user = JSON.parse(json);
-	if (!user.name) {
-		throw new SyntaxError('Unfortunately haven`t name parametr'); // We can use all standart errors form JS
-	}
-} catch(error) {
-	console.log(error);
-} finally {
-	console.log('I work always!');
+	this.gatAge = function() {
+		return userAge;
+	};
+
+	this.setAge = function(age) {
+		if (typeof age == 'number' && age > 0 && age < 110) {
+			userAge = age;
+		} else {
+			throw new Error("Incorret value of age");
+		}
+	};
+
+	this.say = function () {
+		console.log(`User's name is ${this.name}. Hi's ${this.userAge} years old.`);
+	};
 }
 
-console.log('Script is working late');
+let user = new User('Ivan', 25);
+
+console.log(user.name);
+console.log(user.userAge);
+user.say();
+console.log(user.gatAge());
+user.setAge('30');
+console.log(user.gatAge());
