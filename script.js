eaa415d5a94d3d4cac238a72343141ef6b2f502e
Anar Agaev <anar.n.agaev@gmail.com>
2019-10-03 14:16:50 +0300

    модули

--- script.js
+++ script.js
@@ -1,31 +1,69 @@
 'use strict';
 
-function User(name, age) {
-	this.name = name;
-	let userAge = age;
+// let app = {
+// 	data: 4,
+// };
 
-	this.gatAge = function() {
-		return userAge;
+// console.log(app);
+
+
+// function User(name, age) {
+// 	this.name = name;
+// 	let userAge = age;
+
+// 	this.gatAge = function() {
+// 		return userAge;
+// 	};
+
+// 	this.setAge = function(age) {
+// 		if (typeof age == 'number' && age > 0 && age < 110) {
+// 			userAge = age;
+// 		} else {
+// 			throw new Error("Incorret value of age");
+// 		}
+// 	};
+
+// 	this.say = function () {
+// 		console.log(`User's name is ${this.name}. Hi's ${this.userAge} years old.`);
+// 	};
+// }
+
+// let user = new User('Ivan', 25);
+
+// console.log(user.name);
+// console.log(user.userAge);
+// user.say();
+// console.log(user.gatAge());
+// user.setAge('30');
+// console.log(user.gatAge());
+
+
+// let number = 1;
+
+// (function() {
+// 	let number = 2;
+// 	console.log(number);
+
+// 	return console.log(number + 3);
+// })();
+
+// console.log(number);
+
+let user = (function() {
+	let privat = function() {
+		console.log('I am pravet.');
 	};
 
-	this.setAge = function(age) {
-		if (typeof age == 'number' && age > 0 && age < 110) {
-			userAge = age;
-		} else {
-			throw new Error("Incorret value of age");
-		}
+	let	sayHello = function() {
+		console.log('Hello!');
 	};
 
-	this.say = function () {
-		console.log(`User's name is ${this.name}. Hi's ${this.userAge} years old.`);
+	return {
+		sayHello: sayHello,
 	};
-}
+}());
 
-let user = new User('Ivan', 25);
+console.log(user);
+console.log(user.sayHello());
 
-console.log(user.name);
-console.log(user.userAge);
-user.say();
-console.log(user.gatAge());
-user.setAge('30');
-console.log(user.gatAge());
+
